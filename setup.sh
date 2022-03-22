8337812325f6dda06aaf088a39a4a708b5ef5fdd
aukey2 <aukey2@illinois.edu>
2022-03-22 11:32:09 -0500

    Delete all

--- setup.sh
+++
@@ -1,7 +0,0 @@
-mkdir logs
-
-conda create -n simpletransformers python pandas tqdm
-conda activate simpletransformers
-# conda install pytorch cudatoolkit=10.2 -c pytorch
-
-pip install -r requirements.txt
