6e4dd91711e589d4b003b9a0a0cc89eda4815f50
aukey2 <aukey2@illinois.edu>
2022-08-25 01:20:48 -0400

    Delete explore files

--- src/data-prep/explore_embs.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import pdb
-
-embs_dict_path = "data/word2vec_embs/w2v_embs_dict.pickle"
-
-if __name__ == '__main__':
-    with open(embs_dict_path, 'rb') as f:
-        word_to_emb = pickle.load(f)
-        pdb.set_trace()
--- src/data-prep/explore_tok2embs.py
+++
@@ -1,9 +0,0 @@
-import pickle
-import pdb
-
-embs_dict_path = "data/word2vec_embs/tok2embs_dict.pickle"
-
-if __name__ == '__main__':
-    with open(embs_dict_path, 'rb') as f:
-        tok_to_embs = pickle.load(f)
-        pdb.set_trace()
